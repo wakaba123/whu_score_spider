016995fcc46fd78ed5107961802718dc8459bf13
wuruoye <2019302180149@whu.edu.cn>
2021-01-26 10:16:21 +0800

    5min

--- spider2.py
+++ spider2.py
@@ -3,16 +3,14 @@ from bs4 import BeautifulSoup
 from PIL import Image
 import re
 import time
-import hashlib
+import os
 
 
 class Spider(object):
-    def __init__(self, your_id, your_pwd):
+    def __init__(self):
         self.url = "http://bkjw.whu.edu.cn/servlet/_6daf195df2a"  # 用于重定向的url
         self.refer = 'http://bkjw.whu.edu.cn'  # 教务系统限定了refer是这个,并且也可以作为一些请求的前半部分
         self.s = requests.session()
-        self.id = your_id
-        self.pwd = your_pwd
         self.s.headers.update({"Connection": "keep-alive",
                                "Upgrade-Insecure-Requests": "1",
                                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
@@ -25,20 +23,20 @@ class Spider(object):
         self.login_data = {
             "timestamp": "1610880036436",
             "jwb": "%E6%AD%A6%E5%A4%A7%E6%9C%AC%E7%A7%91%E6%95%99%E5%8A%A1%E7%B3%BB%E7%BB%9F",
-            "id": self.id,
-            "pwd": self.getHashPwd(),
+            "id": "2019302180149",
+            "pwd": "1a0a11a1093806e8883ee1f07dbb2422",
             "xdvfb": ""  # 提交的表单的数据
         }
-
-    def getHashPwd(self):
-        return hashlib.md5(self.pwd.encode(encoding='UTF-8')).hexdigest()
 
     def getCaptcha(self):
         r = self.s.get(self.url)  # 第一次请求获取验证码的地址
         demo = BeautifulSoup(r.text, 'html.parser')
         a = demo.find_all('img', attrs={'name': "sleep"})
+        print(a)
         captcha_img = a[0].attrs['src']
+
         captcha = self.refer + captcha_img  # checkcode是验证码的网址
+        # print(captcha)
         return captcha
 
     def inputCaptcha(self):
@@ -53,9 +51,12 @@ class Spider(object):
         self.inputCaptcha()
         r = self.s.post(self.url, data=self.login_data)
         if len(r.text) != 17906:
-            print("验证码错误")
+            print(r.text)
             return False
         else:
+            im = Image.open('checked.gif')
+            im.save(self.login_data['xdvfb'] + '.tiff')
+            os.remove('checked.gif')
             return True
 
     def getScorePage(self):
@@ -74,12 +75,13 @@ class Spider(object):
         a = ' '.join(a)
 
         url_crfs = self.refer + url_crfs + a  # 获得真正可以使用的网址
+        print(url_crfs)
         r2 = self.s.get(url_crfs)  # 访问该网址,获得成绩信息
         return r2.text
 
     def processScore(self, year, term):
         demo2 = BeautifulSoup(self.getScorePage(), "html.parser")  # 以下内容为用beautifulsoup提取成绩信息.
-
+        score_not_got = 0
         a = demo2.find_all('tr')
         if len(a) == 0:
             print('未知错误')
@@ -90,19 +92,23 @@ class Spider(object):
             if term in termData and year in yearData:
                 lessonName = all_tds[0].string.replace('\n', '') + '\n'
                 lessonScore = all_tds[-2].string
+                if lessonScore is None:
+                    score_not_got += 1
                 print(lessonName, lessonScore)
+        return score_not_got
 
 
-id = '2019302180xxx'  # 这里输入你的学号
-pwd = 'xxxxxx'        # 这里输入你的密码
-spider = Spider(id, pwd)
+spider = Spider()
+
+# while True:
+#     spider.login()
 
 while spider.login() is False:
     pass
 
-while input("是否继续查询(y/n):") != 'n':
-    year = input("年份:")
-    term = input("学期:")
-    # year = '2020'
-    # term = '1'
-    spider.processScore(year, term)
+while True:
+    year = '2020'
+    term = '1'
+    print("没出的成绩有%d门" % spider.processScore(year, term))
+    print(time.asctime())
+    time.sleep(300)
